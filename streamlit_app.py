a5deb39f30c2cbe561529d798f925d10dcbcd09c
swap2728 <98693780+swap2728@users.noreply.github.com>
2023-10-31 18:27:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 data = pd.read_csv("books.csv",sep="\t")
 
 # Create interaction matrix and find similarity
-interaction_matrix = data.pivot_table(index='User ID', columns='Product ID', values='Rating', fill_value=0)
+interaction_matrix = data.pivot_table(index='bookID', columns='title', values='average_rating', fill_value=0)
 product_similarity = cosine_similarity(interaction_matrix.T)
 
 # Function to get product recommendations based on product name and category
@@ -24,45 +24,45 @@ def get_recommendations(user_id, product_name, category, interaction_matrix, pro
 
 # Function to filter recommended products by product name and category
 def filter_by_product_name_and_category(products, product_name, category):
-    filtered_products = data[data['Product Name'] == product_name]
-    filtered_products = filtered_products[filtered_products['Category'] == category]
+    filtered_products = data[data['title'] == product_name]
+    filtered_products = filtered_products[filtered_products['authors'] == category]
     return filtered_products
 
 # Streamlit app
 def main():
     
     st.set_page_config(
-        page_title="Fashion Product Recommender",
+        page_title="Books Recommender",
         page_icon="🛍️",
         
     )
 
     
-    st.title("Fashion Product Recommender")
-    st.markdown("Discover personalized fashion product recommendations.")
+    st.title("Books Recommender")
+    st.markdown("Discover personalized Book Recommendations.")
     
     # User input
     user_id = st.number_input("Enter User ID", min_value=1, max_value=1000)
-    product_name = st.selectbox("Select Product Name", data['Product Name'].unique())
-    category = st.selectbox("Select Category", data['Category'].unique())
+    product_name = st.selectbox("Select Product Name", data['title'].unique())
+    category = st.selectbox("Select Category", data['authors'].unique())
    
     # Recommendation button
     if st.button("Get Recommendations"):
         recommendations = get_recommendations(user_id, product_name, category, interaction_matrix, product_similarity)
         
         if len(recommendations) > 10:
-            random_recommendations = random.sample(list(recommendations['Product ID']), 10)
+            random_recommendations = random.sample(list(recommendations['bookID']), 10)
         else:
-            random_recommendations = list(recommendations['Product ID'])
+            random_recommendations = list(recommendations['bookID'])
         
         # Display recommended products
         st.subheader("Recommended Products:")
-        recommended_products_info = data[data['Product ID'].isin(random_recommendations)][['Product ID', 'Product Name', 'Category', 'Brand', 'Color']]
+        recommended_products_info = data[data['bookID'].isin(random_recommendations)][['bookID', 'title', 'authors', 'language_code', 'publisher']]
         st.table(recommended_products_info)
         
         # Display user's history
         st.subheader("User History:")
-        user_products_info = data[data['User ID'] == user_id][['Product Name', 'Category', 'Brand', 'Color']].drop_duplicates()
+        user_products_info = data[data['User ID'] == user_id][['title', 'authors', 'language_code', 'publisher']].drop_duplicates()
         st.table(user_products_info)
 
 if __name__ == "__main__":
